66559028ff121a90d99dc90a7b37c523585bda36
Cnfc <stdashkov@mail.ru>
2019-06-14 16:33:39 +0300

    del older & add yarn package to ork with redux and tests

---
+++ src/components/App/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+          Start to build personal site
+      </header>
+    </div>
+  );
+}
+
+export default App;

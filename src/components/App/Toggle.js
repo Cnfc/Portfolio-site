b86d0e4735a1d7e1d775cf83f09d964093e4502a
Cnfc <stdashkov@mail.ru>
2019-07-15 20:00:20 +0200

    add transition button

--- src/components/App/Toggle.js
+++ src/components/App/Toggle.js
@@ -1,27 +1,89 @@
 import React, { useState } from "react";
-import { useSpring, animated } from "react-spring";
+import { useTransition, animated } from "react-spring";
 
 const Toggle = () => {
-  const [toggle, setTogggle] = useState(false);
-  const { color, y } = useSpring({
-    // opacity: toggle ? 1 : 0,
-    // fontSize: toggle ? "5rem" : "1rem",
-    color: toggle ? "tomato" : "green",
-    y: toggle ? -500 : 0
+  const [items, setItems] = useState([
+    {
+      letter: "S",
+      key: 1
+    },
+    {
+      letter: "c",
+      key: 2
+    },
+    {
+      letter: "o",
+      key: 3
+    },
+    {
+      letter: "t",
+      key: 4
+    },
+    {
+      letter: "t",
+      key: 5
+    }
+  ]);
+  debugger;
+
+  const transition = useTransition(items, item => item.key, {
+    from: { opacity: 0 },
+    enter: { opacity: 1 },
+    leave: { opacity: 0 }
   });
+  // ===================== JS =====================
+
+  // ====================== JS ========================
 
   return (
-    <div className="" onClick={() => setTogggle(!toggle)}>
-      <animated.h1
-        style={{
-          transform: y.interpolate(y => `translate3d(${y}px ,0, 0)`),
-          color
-        }}
+    <div>
+      {transition.map(({ item, key, props }) => (
+        <animated.h1 key={key} style={props}>
+          {item.letter}
+        </animated.h1>
+      ))}
+
+      <button
+        onClick={() =>
+          setItems([
+            {
+              letter: "S",
+              key: 1
+            }
+          ])
+        }
       >
-        Hello
-      </animated.h1>
+        Toggle
+      </button>
     </div>
   );
 };
 
 export default Toggle;
+
+// const transition = useTransition(toggle, null, {
+//   from: { opacity: 0, position: "absolute" },
+//   enter: { opacity: 1 },
+//   leave: { opacity: 0 }
+// });
+
+// return (
+//   <div
+//     style={{
+//       position: "relative"
+//     }}
+//   >
+//     {transition.map(({ item, key, props }) =>
+//       item ? (
+//         <animated.h1 key={key} style={props}>
+//           Hello
+//         </animated.h1>
+//       ) : (
+//         <animated.h1 style={props}>World</animated.h1>
+//       )
+//     )}
+
+//     <button onClick={() => setTogggle(!toggle)}>Toggle</button>
+//   </div>
+// );
+// };
